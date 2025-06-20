6d9504704a38c7fb1a6a5e40dceeb201d0728bfb
Aayush8356 <61739992+Aayush8356@users.noreply.github.com>
2025-06-20 22:18:54 +0545

    Update start.sh

--- server/start.sh
+++ server/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Running PostgreSQL Migrations..."
-psql "$DATABASE_URL" -f ./server/database/migrations/create_locations_best_time.sql
+psql "$DATABASE_URL" -f ./database/migrations/create_locations_best_time.sql
 
 echo "Running PostgreSQL Seed..."
-psql "$DATABASE_URL" -f ./server/database/seeds/seed_locations_best_time.sql
+psql "$DATABASE_URL" -f ./database/seeds/seed_locations_best_time.sql
 
 echo "Starting Node App..."
 npm run start
